6a30c82eac04e3dd48f6b1a80625c38ce16d2776
Your Name <you@example.com>
2022-05-05 11:54:56 -0400

    new try

--- heap.c
+++ heap.c
@@ -48,7 +48,7 @@ void heap_push(Heap* pq, void* data, int priority){
          pq->heapArray[i].data = data;
          pq->heapArray[i].priority = priority;
          index = i;
-         printf("%d  ", pq->heapArray[i].priority);
+         printf("%d  %d", pq->heapArray[i].priority, index);
          break;
       }
    }
