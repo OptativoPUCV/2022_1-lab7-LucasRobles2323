39149c6c417fc3eb1e34aa888f0d6ab76d567f5a
Your Name <you@example.com>
2022-05-05 12:15:08 -0400

    new try

--- heap.c
+++ heap.c
@@ -29,7 +29,15 @@ void heap_push(Heap* pq, void* data, int priority){
    if(pq->size == pq->capac){
       pq->capac = 2 * pq->size;
       pq->capac += 1;
-      pq->heapArray = (heapElem*) realloc (pq->heapArray, sizeof(heapElem) * pq->capac);
+      heapElem *heapArray = (heapElem*) calloc (pq->capac, sizeof(heapElem));
+      for (size_t i = 0; i < pq->capac; i++)
+      {
+         if(pq->size > i){
+            heapArray->data = pq->heapArray[i].data;
+            heapArray->priority =pq->heapArray[i].priority;
+         }
+      }
+      
    }
 
    for (size_t i = 0; i < pq->capac; i++)
@@ -38,6 +46,7 @@ void heap_push(Heap* pq, void* data, int priority){
          return;
       }
    }
+
    int index;
    for (size_t i = 0; i < pq->capac; i++)
    {
