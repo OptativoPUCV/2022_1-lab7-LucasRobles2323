8688d1c362c0bd3e3561b00cbee5fff8837ac41d
Your Name <you@example.com>
2022-05-06 19:36:19 -0400

    new try

--- heap.c
+++ heap.c
@@ -71,7 +71,6 @@ void heap_push(Heap* pq, void* data, int priority){
 void heap_pop(Heap* pq){
    if(pq->size == 0){return;}
    pq->heapArray[0].priority = 0;
-   if(pq->size == 1){return;}
    
    heapElem aux;
    aux = pq->heapArray[0];
